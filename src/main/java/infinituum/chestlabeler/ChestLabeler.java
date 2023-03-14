1d37a82ad2d7efd88437b331e57e41f41bf1d085
Infinituum17 <riccardomele1706@gmail.com>
2023-03-14 23:02:42 +0100

    [🎉] First commit!

---
+++ src/main/java/infinituum/chestlabeler/ChestLabeler.java
@@ -0,0 +1,23 @@
+package infinituum.chestlabeler;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChestLabeler implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MODID = "chestlabeler";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
