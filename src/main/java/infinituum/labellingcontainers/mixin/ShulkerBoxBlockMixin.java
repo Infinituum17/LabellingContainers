71604cca4e8cf5ffd266e832e9b3a483991f6152
Infinituum17 <riccardomele1706@gmail.com>
2023-07-02 15:09:03 +0200

    [✨] Fixed compatibility issues with TweakerMore mod

--- src/main/java/infinituum/labellingcontainers/mixin/ShulkerBoxBlockMixin.java
+++ src/main/java/infinituum/labellingcontainers/mixin/ShulkerBoxBlockMixin.java
@@ -7,8 +7,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.BlockView;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -16,8 +18,8 @@ import java.util.List;
 public class ShulkerBoxBlockMixin extends Block {
     public ShulkerBoxBlockMixin(Settings settings) { super(settings); }
 
-    @Override
-    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+    @Inject(method = "appendTooltip", at = @At("TAIL"))
+    public void appendTooltip(ItemStack stack, BlockView world, List<Text> tooltip, TooltipContext options, CallbackInfo ci) {
         tooltip.add(Text.translatable("block.labelable").formatted(Formatting.ITALIC).formatted(Formatting.GRAY));
 
         super.appendTooltip(stack, world, tooltip, options);
