586f0a03c800359d8c1bd06681455bdaa99a21e8
Infinituum17 <riccardomele1706@gmail.com>
2023-07-04 18:43:20 +0200

    [✨] Changes: - Added compatibility with "Iron Chests: Restocked" mod - Removed modrinth gradle plugin - Added unifiedPublishing gradle plugin - Made "labelable" tooltip a little prettier

--- src/main/java/infinituum/labellingcontainers/mixin/ChestBlockMixin.java
+++ src/main/java/infinituum/labellingcontainers/mixin/ChestBlockMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
@@ -43,7 +44,9 @@ public class ChestBlockMixin extends Block {
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
-        tooltip.add(Text.translatable("block.labelable").formatted(Formatting.ITALIC).formatted(Formatting.GRAY));
+        MutableText text = Text.literal("ⓘ ").formatted(Formatting.BLUE);
+        text.append(Text.translatable("block.labelable").formatted(Formatting.ITALIC).formatted(Formatting.GRAY));
+        tooltip.add(text);
 
         super.appendTooltip(stack, world, tooltip, options);
     }
--- src/main/java/infinituum/labellingcontainers/mixin/GenericChestBlockMixin.java
+++ src/main/java/infinituum/labellingcontainers/mixin/GenericChestBlockMixin.java
@@ -1,26 +1,26 @@
 package infinituum.labellingcontainers.mixin;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tech.thatgravyboat.ironchests.common.blocks.GenericChestBlock;
 
 import java.util.List;
 
-@Mixin(ShulkerBoxBlock.class)
-public class ShulkerBoxBlockMixin extends Block {
-    public ShulkerBoxBlockMixin(Settings settings) { super(settings); }
+@Mixin(GenericChestBlock.class)
+public class GenericChestBlockMixin extends Block {
+    public GenericChestBlockMixin(Settings settings) {
+        super(settings);
+    }
 
-    @Inject(method = "appendTooltip", at = @At("TAIL"))
-    public void appendTooltip(ItemStack stack, BlockView world, List<Text> tooltip, TooltipContext options, CallbackInfo ci) {
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
         MutableText text = Text.literal("ⓘ ").formatted(Formatting.BLUE);
         text.append(Text.translatable("block.labelable").formatted(Formatting.ITALIC).formatted(Formatting.GRAY));
         tooltip.add(text);
