6c30f0221d9426cc45ff76f55171a4e8b0bccb6e
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-01-21 16:53:43 +0100

    🧨 Added Custom Logger, Fixed #7

--- common/src/main/java/infinituum/labellingcontainers/LabellingContainers.java
+++ common/src/main/java/infinituum/labellingcontainers/LabellingContainers.java
@@ -4,11 +4,15 @@ import infinituum.labellingcontainers.registration.CommandRegistration;
 import infinituum.labellingcontainers.registration.EventHandlersRegistration;
 import infinituum.labellingcontainers.registration.ItemRegistration;
 import infinituum.labellingcontainers.registration.ScreenRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LabellingContainers {
     public static final String MOD_ID = "labellingcontainers";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     
     public static void init() {
+        LOGGER.info("Initializing mod...");
         CommandRegistration.init();
         ItemRegistration.init();
         ScreenRegistration.init();
