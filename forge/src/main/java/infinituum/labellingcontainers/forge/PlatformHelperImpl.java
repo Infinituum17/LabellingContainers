784a416e660855a97447f6ddc211ef6c11c8cb64
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-02-25 15:11:57 +0100

    🔧 Fixed #21

--- forge/src/main/java/infinituum/labellingcontainers/forge/PlatformHelperImpl.java
+++ forge/src/main/java/infinituum/labellingcontainers/forge/PlatformHelperImpl.java
@@ -1,22 +1,25 @@
 package infinituum.labellingcontainers.forge;
 
+import dev.architectury.platform.Platform;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.world.World;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.cyclops.colossalchests.block.ChestWall;
 import org.cyclops.colossalchests.block.ColossalChest;
 
 public class PlatformHelperImpl {
     public static BlockEntity locateTargetBlockEntity(World world, BlockPos blockPos) {
-        BlockState blockState = world.getBlockState(blockPos);
-        Block block = blockState.getBlock();
+        if(Platform.isModLoaded("colossalchests")) {
+            BlockState blockState = world.getBlockState(blockPos);
+            Block block = blockState.getBlock();
 
-        if(block instanceof ChestWall && blockState.get(ColossalChest.ENABLED)) {
-            BlockPos corePos = ColossalChest.getCoreLocation(((ChestWall) block).getMaterial(), world, blockPos);
+            if(block instanceof ChestWall && blockState.get(ColossalChest.ENABLED)) {
+                BlockPos corePos = ColossalChest.getCoreLocation(((ChestWall) block).getMaterial(), world, blockPos);
 
-            if(corePos != null) return world.getBlockEntity(corePos);
+                if(corePos != null) return world.getBlockEntity(corePos);
+            }
         }
 
         return world.getBlockEntity(blockPos);
