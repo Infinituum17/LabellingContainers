ba39319a672b842e51f9f1aa3efb97de3e87dc62
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-02-25 16:35:15 +0100

    🔧 Implemented PlatformHelper for easier Block Entity checks in the future

--- common/src/main/java/infinituum/labellingcontainers/PlatformHelper.java
+++ common/src/main/java/infinituum/labellingcontainers/PlatformHelper.java
@@ -1,4 +1,19 @@
 package infinituum.labellingcontainers;
 
+import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
 public class PlatformHelper {
+    /**
+     * Locates the correct BlockEntity based on platform-specific interactions with mods.
+     * @param world The World where we want to locate this BlockEntity.
+     * @param blockPos The block position we are starting this operation from.
+     * @return A BlockEntity or <code>null</code> if none is found.
+     */
+    @ExpectPlatform
+    public static BlockEntity locateTargetBlockEntity(Level world, BlockPos blockPos) {
+        throw new AssertionError();
+    }
 }
--- fabric/src/main/java/infinituum/labellingcontainers/fabric/PlatformHelperImpl.java
+++ fabric/src/main/java/infinituum/labellingcontainers/fabric/PlatformHelperImpl.java
@@ -1,4 +1,11 @@
 package infinituum.labellingcontainers.fabric;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
 public class PlatformHelperImpl {
+    public static BlockEntity locateTargetBlockEntity(Level world, BlockPos blockPos) {
+        return world.getBlockEntity(blockPos);
+    }
 }
