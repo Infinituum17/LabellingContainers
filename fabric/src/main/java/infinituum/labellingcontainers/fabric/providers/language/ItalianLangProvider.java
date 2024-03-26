9594a3d6a71464b6bc200a0e538e0cb0f94f571f
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-26 01:46:30 +0100

    🔨 Added "/labelconfig remove-..." command

--- fabric/src/main/java/infinituum/labellingcontainers/fabric/providers/language/ItalianLangProvider.java
+++ fabric/src/main/java/infinituum/labellingcontainers/fabric/providers/language/ItalianLangProvider.java
@@ -22,7 +22,8 @@ public class ItalianLangProvider extends FabricLanguageProvider {
         translationBuilder.add(labelPrinterKey + ".tooltip.description", "Usa la carta per stampare nuove etichette");
         translationBuilder.add(labelPrinterKey + ".paper.error", "Non hai abbastanza carta!");
         translationBuilder.add(labelPrinterKey + ".untaggable.error", "Non puoi etichettare questo blocco!");
-        translationBuilder.add("command.labelconfig.success", "%s è stato aggiunto al config con successo!");
+        translationBuilder.add("command.labelconfig.addition.success", "%s è stato aggiunto al config con successo!");
+        translationBuilder.add("command.labelconfig.removal.success", "%s è stato rimosso dal config con successo!");
 
         translationBuilder.add("block.labelable", "Usa una Stampante di etichette per etichettare questo blocco");
     }
