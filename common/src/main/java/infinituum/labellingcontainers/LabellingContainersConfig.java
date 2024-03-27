949d307943be82ca459b2549740a200364a6d8af
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-27 12:21:42 +0100

    🗒️ Added ability to change HUD position

--- common/src/main/java/infinituum/labellingcontainers/LabellingContainersConfig.java
+++ common/src/main/java/infinituum/labellingcontainers/LabellingContainersConfig.java
@@ -3,18 +3,30 @@ package infinituum.labellingcontainers;
 import dev.architectury.platform.Platform;
 import infinituum.fastconfigapi.api.FastConfig;
 import infinituum.fastconfigapi.api.FastConfigFile;
-import infinituum.labellingcontainers.config.TaggableBlocksConfig;
+import infinituum.labellingcontainers.config.PlayerPreferences;
+import infinituum.labellingcontainers.config.TaggableBlocks;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import static infinituum.labellingcontainers.LabellingContainers.MOD_ID;
 
 public class LabellingContainersConfig {
     public static FastConfig MAIN_CONFIG;
     public static FastConfig MOD_CONFIG;
 
-    public static FastConfigFile<TaggableBlocksConfig> TAGGABLE_BLOCKS_CONFIG;
+    public static FastConfigFile<TaggableBlocks> TAGGABLE_BLOCKS_CONFIG;
+    @Environment(EnvType.CLIENT)
+    public static FastConfigFile<PlayerPreferences> PLAYER_PREFERENCES_CONFIG;
 
     public static void init() {
-        MAIN_CONFIG = new FastConfig(Platform.getConfigFolder());
+        MAIN_CONFIG = new FastConfig(Platform.getConfigFolder(), MOD_ID);
         MOD_CONFIG = MAIN_CONFIG.getSubDirConfig();
 
-        TAGGABLE_BLOCKS_CONFIG = MAIN_CONFIG.getConfigFile(TaggableBlocksConfig.class);
+        TAGGABLE_BLOCKS_CONFIG = MOD_CONFIG.getConfigFile(TaggableBlocks.class);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static void initClient() {
+        PLAYER_PREFERENCES_CONFIG = MOD_CONFIG.getConfigFile(PlayerPreferences.class);
     }
 }
