2846548eb08b81cad5b1d120e799ef3a60250d8a
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-27 12:21:42 +0100

    🗒️ Added ability to change HUD position

--- common/src/main/java/infinituum/labellingcontainers/network/Packets.java
+++ common/src/main/java/infinituum/labellingcontainers/network/Packets.java
@@ -1,9 +1,10 @@
 package infinituum.labellingcontainers.network;
 
-import static infinituum.labellingcontainers.LabellingContainers.MOD_ID;
-
 import net.minecraft.resources.ResourceLocation;
+
+import static infinituum.labellingcontainers.LabellingContainers.MOD_ID;
 
 public class Packets {
     public static final ResourceLocation LABEL_UPDATE_PACKET_ID = new ResourceLocation(MOD_ID, "label_update_packet");
+    public static final ResourceLocation PLAYER_PREFERENCES_CONFIG_UPDATE = new ResourceLocation(MOD_ID, "player_preferences_config_update");
 }
