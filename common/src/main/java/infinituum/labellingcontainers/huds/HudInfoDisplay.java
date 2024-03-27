ae24a5ba12ecac8b83ab3fc876a83f465fb8d723
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-27 12:21:42 +0100

    🗒️ Added ability to change HUD position

--- common/src/main/java/infinituum/labellingcontainers/huds/HudInfoDisplay.java
+++ common/src/main/java/infinituum/labellingcontainers/huds/HudInfoDisplay.java
@@ -2,16 +2,16 @@ package infinituum.labellingcontainers.huds;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.architectury.event.events.client.ClientGuiEvent.RenderHud;
+import infinituum.labellingcontainers.huds.utils.HUDPositions;
 import infinituum.labellingcontainers.utils.BlockEntityHelper;
 import infinituum.labellingcontainers.utils.Taggable;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+
+import static infinituum.labellingcontainers.LabellingContainersConfig.PLAYER_PREFERENCES_CONFIG;
 
 public class HudInfoDisplay implements RenderHud {
     @Override
@@ -28,23 +28,10 @@ public class HudInfoDisplay implements RenderHud {
 
         BlockEntity blockEntity = BlockEntityHelper.locateTargetBlockEntity(client.level, blockPos, client.level.getBlockState(blockPos));
 
-        if(blockEntity instanceof Taggable labelable) {
-            Item displayItem = (labelable.labellingcontainers$getDisplayItem() != null) ? labelable.labellingcontainers$getDisplayItem() : client.level.getBlockState(blockPos).getBlock().asItem();
+        if (blockEntity instanceof Taggable taggable) {
+            HUDPositions position = PLAYER_PREFERENCES_CONFIG.get().getHUDPosition();
 
-            int width = client.getWindow().getGuiScaledWidth();
-            int height = client.getWindow().getGuiScaledHeight();
-            int fontHeight = client.font.lineHeight;
-
-            int x = width / 2;
-            int y = height / 2;
-            int leftPadding = 10;
-
-            context.pushPose();
-
-            client.getItemRenderer().renderGuiItem(context, new ItemStack(displayItem), x + leftPadding, y - 8);
-            GuiComponent.drawString(context, client.font, labelable.labellingcontainers$getLabel(), x + leftPadding * 3, (y + 1) - fontHeight / 2, 0xFFFFFFFF);
-
-            context.popPose();
+            position.render(client, context, taggable);
         }
     }
 }
