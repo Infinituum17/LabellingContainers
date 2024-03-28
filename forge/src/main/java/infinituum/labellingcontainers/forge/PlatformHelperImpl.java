9098438eb49d0dfe9e213dda5652b9c985c5d4dc
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-28 21:13:47 +0100

    🔧 Fixed cherry-pick and refactoring

--- forge/src/main/java/infinituum/labellingcontainers/forge/PlatformHelperImpl.java
+++ forge/src/main/java/infinituum/labellingcontainers/forge/PlatformHelperImpl.java
@@ -1,7 +1,5 @@
 package infinituum.labellingcontainers.forge;
 
-import dev.architectury.platform.Platform;
-import infinituum.labellingcontainers.forge.handlers.ColossalChestsHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -10,10 +8,6 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class PlatformHelperImpl {
     public static BlockEntity locateTargetBlockEntity(Level level, BlockPos blockPos, BlockState blockState) {
-        if(Platform.isModLoaded("colossalchests")) {
-            return ColossalChestsHandler.handle(level, blockPos, blockState);
-        }
-
         return level.getBlockEntity(blockPos);
     }
 }
