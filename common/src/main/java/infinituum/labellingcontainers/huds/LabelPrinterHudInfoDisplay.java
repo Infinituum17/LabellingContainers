dfc07fc998baa1f5694c90568dae0bc29f2bd0a5
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-03-29 01:16:10 +0100

    🔧 Fixed cherry-pick file

--- common/src/main/java/infinituum/labellingcontainers/huds/LabelPrinterHudInfoDisplay.java
+++ common/src/main/java/infinituum/labellingcontainers/huds/LabelPrinterHudInfoDisplay.java
@@ -1,17 +1,16 @@
 package infinituum.labellingcontainers.huds;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import dev.architectury.event.events.client.ClientGuiEvent.RenderHud;
 import infinituum.labellingcontainers.items.LabelPrinterItem;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
 
 public class LabelPrinterHudInfoDisplay implements RenderHud {
     @Override
-    public void renderHud(PoseStack matrices, float tickDelta) {
+    public void renderHud(GuiGraphics graphics, float tickDelta) {
         Minecraft client = Minecraft.getInstance();
         if (client.screen != null || client.player == null) return;
 
@@ -26,11 +25,11 @@ public class LabelPrinterHudInfoDisplay implements RenderHud {
                     .translatable("item.labellingcontainers.label_printer.tooltip.mode")
                     .append(LabelPrinterItem.getMode(itemInHand).getDisplayable());
 
-            matrices.pushPose();
+            graphics.pose().pushPose();
 
-            GuiComponent.drawString(matrices, client.font, mode, padding, height - fontHeight - padding, 0xFFFFFFFF);
+            graphics.drawString(client.font, mode, padding, height - fontHeight - padding, 0xFFFFFFFF);
 
-            matrices.popPose();
+            graphics.pose().popPose();
         }
     }
 }
